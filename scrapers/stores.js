4a33a92e5355284296d9a155377687d05f3db491
Gery437 <doublegil@gmail.com>
2026-05-22 23:02:54 +0300

    Move יש בשכונה to yesh chain (Yesh Hesed sub-brand, distinct color)

--- scrapers/stores.js
+++ scrapers/stores.js
@@ -17,10 +17,10 @@ const BASE_CERBERUS = 'https://url.publishedprices.co.il'
 const BASE_SHUFERSAL = 'http://prices.shufersal.co.il'
 
 // Chain pattern matchers (name, brand, or operator tags)
-// NOTE: "יש בשכונה" and "יש חסד" are both Shufersal sub-brands
+// NOTE: "יש בשכונה" is a Yesh Hesed (יש חסד) neighborhood format — Shufersal group but different brand
 const CHAIN_MATCHERS = [
-  { key: 'shufersal', pattern: /שופרסל|shufersal|יש בשכונה|yesh.?bash?k?una/i },
-  { key: 'yesh',      pattern: /יש חסד|yesh.?h[ae]sed/i },
+  { key: 'shufersal', pattern: /שופרסל|shufersal/i },
+  { key: 'yesh',      pattern: /יש חסד|yesh.?h[ae]sed|יש בשכונה/i },
   { key: 'ramilevi',  pattern: /רמי לוי|rami.?levy/i },
   { key: 'yohananof', pattern: /יוחננוף|yohananof/i },
   { key: 'osher',     pattern: /אושר עד|osher.?ad|חצי חינם|hazi.?hinam/i },
